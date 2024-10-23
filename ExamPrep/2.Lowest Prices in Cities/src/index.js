2358e8e7a3f264306d0d91979d2580253a4b1ba3
Daniel <32326750+DaniGwen@users.noreply.github.com>
2024-10-23 22:19:30 +0300

    solution

--- ExamPrep/2.Lowest Prices in Cities/src/index.js
+++ ExamPrep/2.Lowest Prices in Cities/src/index.js
@@ -5,32 +5,32 @@ var input = ['Sample Town | Sample Product | 1000',
     'Sofia | Peach | 2',
     'New York | Sample Product | 1000.1',
     'New York | Burger | 10'];
-var Town = /** @class */ (function () {
-    function Town(name, product, price) {
-        this.name = name;
-        this.product = product;
-        this.price = price;
-    }
-    return Town;
-}());
 function ProcessLowestProductPrice(input) {
     var towns = [];
-    var _loop_1 = function (entry) {
-        var townArgs = entry.split("|");
-        var town = new Town(townArgs[0], townArgs[1], +townArgs[2]);
-        var existingTown = towns.find(function (x) { return x.name === town.name && x.product === town.product; });
-        if (existingTown) {
-            if (town.price < existingTown.price) {
-                towns.splice(towns.indexOf(existingTown), 1);
-                towns.push(town);
-                return "continue";
-            }
-        }
-        towns.push();
-    };
     for (var _i = 0, input_1 = input; _i < input_1.length; _i++) {
         var entry = input_1[_i];
-        _loop_1(entry);
+        var townArgs = entry.split("|");
+        var town = {
+            name: townArgs[0],
+            product: townArgs[1],
+            price: +townArgs[2]
+        };
+        var existingProductIndex = -1;
+        for (var i = 0; i < towns.length; i++) {
+            if (towns[i].product === town.product) {
+                existingProductIndex = i;
+                break;
+            }
+        }
+        if (existingProductIndex !== -1) {
+            if (town.price < towns[existingProductIndex].price) {
+                towns.splice(existingProductIndex, 1);
+                towns.push(town);
+            }
+        }
+        else {
+            towns.push(town);
+        }
     }
     towns.sort(function (a, b) { return a.price - b.price; });
     return towns;
--- ExamPrep/2.Lowest Prices in Cities/src/index.ts
+++ ExamPrep/2.Lowest Prices in Cities/src/index.ts
@@ -6,37 +6,45 @@ const input: Array<string> = ['Sample Town | Sample Product | 1000',
     'New York | Sample Product | 1000.1',
     'New York | Burger | 10'];
 
-class Town {
-    name?: string;
-    product?: string;
-    price?: number;
-    constructor(name: string, product: string, price: number) {
-        this.name = name;
-        this.product = product;
-        this.price = price;
-    }
-}
+type Town = {
+    name: string;
+    product: string;
+    price: number;
+};
 
 function ProcessLowestProductPrice(input: Array<string>): Array<Town> {
-    let towns: Array<Town> = [];
+    let towns: Town[] = [];
+
     for (const entry of input) {
         let townArgs = entry.split("|");
-        let town = new Town(townArgs[0], townArgs[1], +townArgs[2]);
-        let existingTown = towns.find(x =>x.name === town.name && x.product === town.product);
-        if(existingTown){
-            if(town.price < existingTown.price){
-                towns.splice(towns.indexOf(existingTown),1);
-                towns.push(town);
-                continue;
+        let town: Town = {
+            name: townArgs[0],
+            product: townArgs[1],
+            price: +townArgs[2]
+        };
+
+        let existingProductIndex = -1;
+
+        for (let i = 0; i < towns.length; i++) {
+            if (towns[i].product === town.product) {
+                existingProductIndex = i;
+                break;
             }
         }
 
-        towns.push();
+        if (existingProductIndex !== -1) {
+            if (town.price < towns[existingProductIndex].price) {
+                towns.splice(existingProductIndex, 1);
+                towns.push(town);
+            }
+        } else {
+            towns.push(town);
+        }
     }
 
-    towns.sort((a,b)=> a.price - b.price);
-
+    towns.sort((a, b) => a.price - b.price);
     return towns;
 }
+
 
 console.log(ProcessLowestProductPrice(input));
